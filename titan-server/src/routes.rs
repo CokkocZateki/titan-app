c57cc64786d0ab028d72dad0810440f049040ca5
Michael-James Parsons <mjay.parsons@gmail.com>
2018-07-31 20:33:06 -0400

    Query branches

--- titan-server/src/routes.rs
+++ titan-server/src/routes.rs
@@ -1,9 +1,8 @@
 use super::db::{UnksoMainForums, TitanPrimary};
 use super::models;
-use rocket_contrib::Json;
-use super::schema::{wcf1_user, wcf_user_group, titan_branches};
+use rocket_contrib::Json;/*
 use diesel::RunQueryDsl;
-use diesel::query_dsl::filter_dsl::FindDsl;
+use diesel::query_dsl::filter_dsl::FindDsl;*/
 use diesel::prelude::*;
 
 #[get("/hello")]
@@ -11,22 +10,24 @@ pub fn hello() -> &'static str {
     "Hello from Project Titan!"
 }
 
-joinable!(wcf1_user -> wcf_user_group (user_online_group_id));
+// joinable!(wcf1_user -> wcf_user_group (user_online_group_id));
 
 #[get("/")]
-pub fn search_users(unkso_main: UnksoMainForums, unkso_titan: TitanPrimary) -> Json<models::TitanBranch> {
-    use super::schema::titan_branches::dsl::*;
-    let branch = titan_branches
-        .filter(titan_branches::dsl::is_enabled.eq(true))
-        .load::<models::TitanBranch>(&unkso_titan)
+pub fn search_users(unkso_main: UnksoMainForums, unkso_titan: TitanPrimary) -> Json<Vec<models::TitanBranch>> {
+    use schema::titan_branches::dsl::*;
+
+    let branches = titan_branches
+        .filter(is_enabled.eq(true))
+        .load::<models::TitanBranch>(&*unkso_titan)
         .unwrap();
 
-    Json(branch[0])
+    Json(branch)
 }
 
 #[get("/<user_id>")]
 pub fn find_user(user_id: i32, unkso_main: UnksoMainForums) -> Json<models::WcfUser> {
-    let user = wcf1_user::table.find(user_id).first(&*unkso_main).unwrap();
+    use schema::wcf1_user::dsl::*;
+    let user = wcf1_user.find(user_id).first(&*unkso_main).unwrap();
 
     Json(user)
 }
